87b0570718b9788ccf2387041b764b6242c06068
ed <edndacomputer@riseup.net>
2023-04-29 03:19:37 -0700

    blog section layout

---
+++ components/Button.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Button = ({ children, size, color, icon }) => {
+  const getSizeClass = (size) => {
+    switch (size) {
+      case 'small':
+        return 'px-2 py-1 text-sm'
+      case 'medium':
+        return 'px-4 py-2 text-base'
+      case 'large':
+        return 'px-6 py-3 text-lg'
+      default:
+        return 'px-4 py-2 text-base'
+    }
+  }
+
+  const getColorClass = (color) => {
+    switch (color) {
+      case 'primary':
+        return 'bg-primary hover:bg-primary-dark text-white'
+      case 'secondary':
+        return 'bg-gray-500 hover:bg-gray-600 text-white'
+      default:
+        return 'bg-blue-500 hover:bg-blue-600 text-white'
+    }
+  }
+
+  return (
+    <button
+      className={`focus:ring-primary inline-flex items-center justify-center rounded-md border border-transparent font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 ${getSizeClass(
+        size
+      )} ${getColorClass(color)}`}
+    >
+      {icon && <Image src={icon} alt="icon" className="mr-2 h-5 w-5" />}
+      {children}
+    </button>
+  )
+}
+
+export default Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,29 +23,29 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="flex justify-center text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+            {/* {siteMetadata.description} */}
           </p>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        <ul className="max-w-4xl mx-auto divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
               <li key={slug} className="py-12">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <div className="space-y-2">
                     <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                       </dd>
                     </dl>
-                    <div className="space-y-5 xl:col-span-3">
+                    <div className="space-y-5 ">
                       <div className="space-y-6">
                         <div>
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
