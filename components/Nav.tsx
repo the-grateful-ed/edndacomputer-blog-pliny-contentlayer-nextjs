adb5c9872ba052e28228ce343cc95c59bb9e141e
edndacomputer <edndacomputer@riseup.net>
2023-05-02 15:49:31 -0700

    fixed header

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,11 +7,10 @@ const Nav = () => {
     { path: '/', label: 'Home' },
     { path: '/about', label: 'About' },
     { path: '/blog', label: 'Blog' },
-    // { path: '/contact', label: 'Contact' },
   ]
   return (
-    <nav className="flex justify-around bg-white py-8">
-      <div className="flex space-x-4">
+    <nav className="flex justify-center bg-white py-8 flex flex-col text-center">
+      <div className="flex space-x-4 mx-auto">
         {links.map(({ path, label }) => (
           <Link key={path} href={path} passHref>
             <div
@@ -24,6 +23,7 @@ const Nav = () => {
           </Link>
         ))}
       </div>
+      <span className='text-xs mx-auto text-pink-500 hover:text-pink-600 hover:scale-110 transition duration-300 pt-4'><a href="mailto:edndacomputer@riseup.net">edndacomputer@riseup.net</a></span>
     </nav>
   )
 }
